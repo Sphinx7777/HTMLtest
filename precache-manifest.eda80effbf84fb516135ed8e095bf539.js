2bfc50ba662474393995afbd8572ca9661ab00c1
Sphinx7777 <spamoglot13@gmail.com>
2019-10-10 20:53:47 +0300

    Updates

---
+++ precache-manifest.eda80effbf84fb516135ed8e095bf539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4271a686879f078238009041110ba54",
+    "url": "/HTMLtest/index.html"
+  },
+  {
+    "revision": "5fd0d0ff975a03e95f3a",
+    "url": "/HTMLtest/static/css/main.a64a42f0.chunk.css"
+  },
+  {
+    "revision": "737bee2a2fe52e38641b",
+    "url": "/HTMLtest/static/js/2.6e9f78cd.chunk.js"
+  },
+  {
+    "revision": "5fd0d0ff975a03e95f3a",
+    "url": "/HTMLtest/static/js/main.7895d0f5.chunk.js"
+  },
+  {
+    "revision": "efd933153674fe58974c",
+    "url": "/HTMLtest/static/js/runtime~main.881ac107.js"
+  }
+]);
